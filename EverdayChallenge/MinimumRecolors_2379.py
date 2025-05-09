b697acd9ced933b6e9e0b40c549c1cb7a6143209
Ipshita Ghosh (THIRD EYE CONSULTING SERVICES) <iuippi3@gmail.com>
2025-05-09 23:58:31 +0530

    added everyday challenge

---
+++ EverdayChallenge/MinimumRecolors_2379.py
@@ -0,0 +1,14 @@
+def minimumRecolors(self, blocks: str, k: int) -> int:
+    n = len(blocks)
+    if n == 0 or k == 0:
+        return 0
+    if n < k:
+        return -1
+    min_recolors = float('inf')
+    for i in range(n - k + 1):
+        recolors = 0
+        for j in range(i, i + k):
+            if blocks[j] == 'W':
+                recolors += 1
+        min_recolors = min(min_recolors, recolors)
+    return min_recolors
