1c9be46bcc1f23296dfd6f255b370791f2826c32
Ipshita Ghosh (THIRD EYE CONSULTING SERVICES) <iuippi3@gmail.com>
2025-05-11 00:18:15 +0530

    solved robot returning to origin

---
+++ ProgrammingSkills/RobotReturn_657.py
@@ -0,0 +1,31 @@
+"""
+Note:
+    This is a simple problem, we just need to check if the robot returns to the origin
+    after moving in the given directions.
+    The robot can move in 4 directions: U, D, L, R
+    U: Up
+    D: Down
+    L: Left
+    R: Right
+    
+    For every move, we can increase or decrease the values of horizontal and vertical coordinates.
+    If the robot returns to the origin, then both values should be 0."""
+
+def judgeCircle(self, moves: str) -> bool:
+    # define 2 values, one for horizontal, one for vertical
+
+    h = 0
+    v = 0
+
+    for move in moves:
+        if move == "U":
+            h += 1
+        if move == "D":
+            h -= 1
+        if move == "R":
+            v += 1
+        if move == "L":
+            v -= 1
+    if h == 0 and v == 0:
+        return True
+    return False
